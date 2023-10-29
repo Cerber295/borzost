689cd6eda387b148c0c071e1b64c902a22812666
Cerber295 <81422454+Cerber295@users.noreply.github.com>
2023-10-29 22:21:40 +0300

    1
    1

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20231020_162825_00_032_pureshot",
+      "name": "IMG_20231020_162825_00_032_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.9275088725250757,
+        "pitch": -0.00005155221474417715,
+        "fov": 1.4185322104917357
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2125723353618874,
+          "pitch": 1.5127701026174378,
+          "rotation": 0,
+          "target": "1-img_20231020_163022_00_033_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20231020_163022_00_033_pureshot",
+      "name": "IMG_20231020_163022_00_033_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7664274307077044,
+          "pitch": 1.546639635060501,
+          "rotation": 0,
+          "target": "0-img_20231020_162825_00_032_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
